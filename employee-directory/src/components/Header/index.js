63b97e03a16641f893cab3a8438ed8753cd8204e
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-06 21:50:54 +0100

    Header component

---
+++ employee-directory/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1><i class="fas fa-sitemap"></i>  Employee Directory  <i class="fas fa-sitemap"></i></h1>
+            <h4>Your Ultimate Employee Lookup</h4>
+        </div>        
+    );
+}
+export default Header
+
