d090968d9c621a5afa3a3d591b1aea2eeb89fede
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-07 19:55:21 +0100

    Call Container component in App.js

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header/index';
+import Container from './components/Container/index'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Container />
     </div>
   );
 }
