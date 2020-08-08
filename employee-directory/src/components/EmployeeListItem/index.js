4d50be376efcb28b5d8c26b2d2e1b6db491282d5
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-08 09:44:39 +0100

    Created employee list items

--- employee-directory/src/components/EmployeeListItem/index.js
+++ employee-directory/src/components/EmployeeListItem/index.js
@@ -1,23 +1,39 @@
 import React from "react";
+import './style.css';
 
 function EmployeeListItem(props) {
     return (
-        <div>
-            <div class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                    <img class="activator" src={props.image} alt="profilepic"></img>
-         </div>
-                    <div class="card-content">
-                        <span class="card-title activator grey-text text-darken-4">Card Title<i class="material-icons right">more_vert</i></span>
-                        <p><a href="#">This is a link</a></p>
-                    </div>
-                    <div class="card-reveal">
-                        <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-                        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+        <div className="col s12 m6 l3">
+            <div className="card">
+                <div className="card-image waves-effect waves-block waves-light">
+                    <img className="activator" src={props.image} alt="profilepic"></img>
+                </div>
+                <div className="card-content">
+                    <span className="card-title activator grey-text text-darken-4">{props.firstname} {props.lastname}<i className="material-icons right">more_vert</i></span>
+    <p><strong>{props.job}</strong></p>
+                </div>
+                <div className="card-reveal">
+                    <span className="card-title grey-text text-darken-4">{props.firstname} {props.lastname}<i className="material-icons right">close</i></span>
+                    <div className="content">
+                        <ul>
+                            <li>
+                                <strong>First Name:</strong> {props.firstname}
+                            </li>
+                            <li>
+                                <strong>Last Name:</strong> {props.lastname}                                
+                            </li>
+                            <li>
+                                <strong>Email:</strong> {props.email}
+                            </li>
+                            <li>
+                                <strong>Department:</strong> {props.job}
+                            </li>                            
+                        </ul>
                     </div>
                 </div>
             </div>
-  );
+        </div>
+    );
 }
 
 export default EmployeeListItem;
