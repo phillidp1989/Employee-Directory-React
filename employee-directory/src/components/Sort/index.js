e6649f7fab46fbc6a1be1bce2d1289bc207f9fdf
Dan Phillips <d.p.phillips@bham.ac.uk>
2020-08-09 22:01:02 +0100

    Sort component

--- employee-directory/src/components/Sort/index.js
+++ employee-directory/src/components/Sort/index.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import './style.css';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
-function Sort(props) {
+export default function Sort(props) {
     return (
-        <div className="sort-container">
-            <div className="input-field col s6">
-                <select onChange={props.handleSort}>
-                    <option value="" defaultValue>How would you like to sort?</option>
-                    <option value="1">Last Name</option>
-                    <option value="2">First Name</option>                    
-                </select>                
-            </div>
-        </div>
-    )
-}
-
-export default Sort
+      <div className="sort-container col s12 m6">
+          <h6>Sort by:</h6>          
+          <FormControl component="fieldset" className="sort">
+        <FormLabel component="legend" className="label"></FormLabel>
+        <RadioGroup row aria-label="position" name="position" defaultValue="top">
+          <FormControlLabel
+            value="First Name"
+            control={<Radio color="primary" />}
+            label="First Name"
+            labelPlacement="top"
+            onChange={props.handleSort}
+          />
+          <FormControlLabel
+            value="Surname"
+            control={<Radio color="primary" />}
+            label="Surname"
+            labelPlacement="top"
+            onChange={props.handleSort}
+          />                 
+        </RadioGroup>
+      </FormControl>
+      </div>      
+    );
+  }
